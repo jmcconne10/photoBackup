586e78c4bcb480a0b1cb4c51ca05a1e7bdc2822e
Joe McConnell <joe.mcconnell10@gmail.com>
2023-12-25 22:35:47 -0600

    This worked as expected

--- unzipFiles.py
+++ unzipFiles.py
@@ -1,23 +1,23 @@
-## TODO
-# Figure out what happens when files share the same name. 
+## This will take zip files and extract them to a folder,
+## while ensuring no duplciates file names are overwritten.
+
+## To use: update the inputDirectory string
+## Once completed, the output will be in the parent folder set for extract_path
 
 #get a list of files in a folder
 import os
 import zipfile
 
-#set the directory you want to unzip files from
-#os.chdir("/Volumes/Video/Google Takeout2023")
-
 # Create a list for duplicates
 duplicates = []
 
-#set directores
-inputDirectory = "./input"
+#set the directory you want to unzip files from
+inputDirectory = "/Volumes/Video/Google Takeout2023"
 os.chdir(inputDirectory)
 
 #set the directory you want to unzip files to
 current_path = os.getcwd()
-extract_path = os.path.dirname(current_path) + "/output"
+extract_path = os.path.dirname(current_path) + "/takeOutoutput"
 os.makedirs(extract_path, exist_ok=True)
 duplicates_path = extract_path + "/duplicates"
 os.makedirs(duplicates_path, exist_ok=True)
@@ -31,14 +31,11 @@ for file in os.listdir():
                 if not os.path.exists(member_path):
                     # File doesn't exist, extract it
                     zip_ref.extract(member, extract_path)
-                    print(f"Extracted: {member}")
+                    #print(f"Extracted: {member}")
                 else:
                     # Add the file to the list of duplicates
                     duplicates.append(member)
-                    zip_ref.extract(member, duplicates_path)
-
-        with zipfile.ZipFile(file, 'r') as zip_ref:
-            zip_ref.extractall()
+                    zip_ref.extract(member, duplicates_path) 
         #os.remove(file)
         print("Zip File: " + os.path.basename(file))
 
