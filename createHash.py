8a5b5c51910edde4be88a8783a18e13a7c1d1319
Joe McConnell <joe.mcconnell10@gmail.com>
2023-12-29 08:53:51 -0600

    added logging

--- createHash.py
+++ createHash.py
@@ -1,10 +1,26 @@
 import hashlib
 import os
 import time
+import logging
+
+# Configure the logging system
+logging.basicConfig(filename='example.log', level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+
+# Create a logger
+logger = logging.getLogger('my_logger')
 
 def hash_file(file_path):
     with open(file_path, 'rb') as f:
-        return hashlib.md5(f.read()).hexdigest()
+        
+        # Try to create a hash object
+        try:
+            hash_object = hashlib.md5(f.read()).hexdigest()
+            logger.debug("File hashed successfully: %s", file_path)
+        except ValueError:
+            hash_object = None
+            logger.error("There was an error hashing the file: %s", file_path)
+
+    return hash_object
 
 # Record start time
 start_time = time.time()
